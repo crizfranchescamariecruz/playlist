b5c41e6a03c6ecf17f236e7cd765ee49c9c802ce
chescaaaa <franchesca.zurc@gmail.com>
2023-10-04 21:08:33 +0800

    main

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/playerr', 'Home::index');
+$routes->post('/create', 'Home::create');
+$routes->get('/playlists/(:any)', 'Home::playlists/$1');
+$routes->get('/search', 'Home::search');
+$routes->post('/upload', 'Home::upload');
+$routes->post('/add', 'Home::add');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,100 @@
 
 namespace App\Controllers;
 
+use App\Models\PlaylistModel;
+use App\Models\MusicModel;
+use App\Models\TracksModel;
+use App\Controllers\BaseController;
+
 class Home extends BaseController
 {
-    public function index(): string
+    private $playlist;
+    private $music;
+    private $tracks;
+
+    public function __construct()
     {
-        return view('welcome_message');
+        $this->playlist = new PlaylistModel();
+        $this->music = new MusicModel();
+        $this->tracks = new TracksModel();
+    }
+    public function index()
+    {
+        $data['playlists'] = $this->playlist->findAll();
+        $data['music'] = $this->music->findAll();
+        return view('playerr', $data);
+    }
+    public function create()
+    {
+        $data = [
+            'name' => $this->request->getPost('name')
+        ];
+        $this->playlist->insert($data);
+        return redirect()->to('/playerr');
+    }
+
+    public function playlists($id)
+    {
+        $playlist = $this->playlist->find($id);
+
+        if ($playlist) {
+            $tracks = $this->tracks->where('p_id', $id)->findAll();
+            $music = [];
+            foreach ($tracks as $track) {
+                $musicItem = $this->music->find($track['t_id']);
+                if ($musicItem) {
+                    $music[] = $musicItem;
+                }
+            }
+            $data = [
+                'playlist' => $playlist,
+                'music' => $music,
+                'playlists' => $this->playlist->findAll(),
+                'tracks' => $tracks,
+            ];
+            return view('playerr', $data);
+        } else {
+            return redirect()->to('/playerr');
+        }
+    }
+
+    public function search()
+    {
+        $search = $this->request->getGet('title');
+        $musicResults = $this->music->like('title', '%' . $search . '%')->findAll();
+        $data = [
+            'playlists' => $this->playlist->findAll(),
+            'music' => $musicResults,
+        ];
+        return view('playerr', $data);
+    }
+    public function add()
+    {
+
+        $musicID = $this->request->getPost('musicID');
+        $playlistID = $this->request->getPost('playlist');
+
+        $data = [
+            'p_id' => $playlistID,
+            't_id' => $musicID,
+        ];
+        $this->tracks->insert($data);
+        return redirect()->to('/playerr');
+    }
+
+    public function upload()
+    {
+        $file = $this->request->getFile('file');
+        $title = $this->request->getPost('title');
+        $artist = $this->request->getPost('artist');
+        $newName = $title . '_' . $artist . '.' . 'mp3';
+        $file->move(ROOTPATH . 'public/', $newName);
+        $data = [
+            'title' => $title,
+            'artist' => $artist,
+            'file_p' => $newName
+        ];
+        $this->music->insert($data);
+        return redirect()->to('/playerr');
     }
 }
